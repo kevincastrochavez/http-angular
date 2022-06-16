171c4179385a00f3681f7ea49d0e3e41896ecc3b
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-15 18:18:36 -0600

    used subjects for error handling when you cannot subscribe to the action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { Post } from "./post.model";
 import { PostsService } from "./post.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private errorSub: Subscription;
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
@@ -18,6 +20,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.isFetching = true;
+
+    this.errorSub = this.postsService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
 
     this.postsService.fetchPosts().subscribe(
       (posts) => {
@@ -56,5 +62,9 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
