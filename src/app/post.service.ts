33fba3062426e0fe843d5c92d6647d40611a19fb
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-17 18:32:52 -0600

    learned how to add query parameters using http

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { map, catchError } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
@@ -39,6 +39,7 @@ export class PostsService {
           headers: new HttpHeaders({
             "Custom-Header": "Hello",
           }),
+          params: new HttpParams().set("print", "pretty"),
         }
       )
       .pipe(
